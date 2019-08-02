1313bccd260f967fd2dd31a75545c2252c735d81
Jonathan Helmus <jjhelmus@gmail.com>
2019-08-02 16:49:21 -0500

    py-mxnet: osx-64 working

--- py-mxnet/build.sh
+++ py-mxnet/build.sh
@@ -1,6 +1,10 @@
 set -x
 
-export MXNET_LIBRARY_PATH=${PREFIX}/lib/libmxnet.so
+if [[ ${HOST} =~ .*linux.* ]]; then
+    export MXNET_LIBRARY_PATH=${PREFIX}/lib/libmxnet.so
+else
+    export MXNET_LIBRARY_PATH=${PREFIX}/lib/libmxnet.dylib
+fi
 
 cd python
 ${PYTHON} setup.py install --with-cython --single-version-externally-managed --record=record.txt
@@ -8,4 +12,8 @@ ${PYTHON} setup.py install --with-cython --single-version-externally-managed --r
 # Delete the copied libmxnet.so and create a relative symlink to $PREFIX/lib/
 # The build scripts produce .so even on osx
 find ${PREFIX} | grep libmxnet.so | grep -v $PREFIX/lib/libmxnet.so | xargs rm -f
-ln -sf ../../../libmxnet.so $SP_DIR/mxnet/libmxnet.so
+if [[ ${HOST} =~ .*linux.* ]]; then
+    ln -sf ../../../libmxnet.so $SP_DIR/mxnet/libmxnet.so
+else
+    ln -sf ../../../libmxnet.dylib $SP_DIR/mxnet/libmxnet.dylib
+fi
